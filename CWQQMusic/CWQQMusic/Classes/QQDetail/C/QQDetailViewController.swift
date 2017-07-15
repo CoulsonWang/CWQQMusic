2c4923e11b9e61a6b830ed1bbf7f1bed7111cd72
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-15 20:20:24 +0800

    布局并设置播放详情界面

--- CWQQMusic/CWQQMusic/Classes/QQDetail/C/QQDetailViewController.swift
+++ CWQQMusic/CWQQMusic/Classes/QQDetail/C/QQDetailViewController.swift
@@ -8,18 +8,74 @@
 
 import UIKit
 
+let screenW = UIScreen.main.bounds.width
+
+
 class QQDetailViewController: UIViewController {
+    
+    @IBOutlet weak var lrcScrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lrcLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    lazy var lrcView = UIView()
+    
+}
 
+extension QQDetailViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addLrcView()
+        setUpLrcScrollView()
     }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setUpLrcView()
+        setUpImageView()
+    }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+// MARK:- 初始化界面
+extension QQDetailViewController {
+    fileprivate func addLrcView() {
+        lrcScrollView.addSubview(lrcView)
+    }
+    fileprivate func setUpLrcView() {
+        var frame = lrcScrollView.bounds
+        frame.origin.x += frame.width
+        lrcView.frame = frame
+        
+        lrcScrollView.contentSize = CGSize(width: 2 * frame.width, height: 0)
+    }
+    fileprivate func setUpLrcScrollView() {
+        lrcScrollView.isPagingEnabled = true
+        lrcScrollView.showsHorizontalScrollIndicator = false
+        lrcScrollView.delegate = self
     }
     
+    fileprivate func setUpImageView() {
+        imageView.layer.cornerRadius = imageView.bounds.width * 0.5
+        imageView.layer.masksToBounds = true
+    }
+    
+    fileprivate func setUpSlider() {
+        slider.setThumbImage(#imageLiteral(resourceName: "player_slider_playback_thumb"), for: .normal)
+        slider.maximumTrackTintColor = UIColor.white
+    }
+}
 
+
+// MARK:- 动画
+extension QQDetailViewController {
+    
+}
+
+
+extension QQDetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let x = scrollView.contentOffset.x
+        
+        imageView.alpha = 1 - x/screenW
+        lrcLabel.alpha = 1 - x/screenW
+    }
 }
